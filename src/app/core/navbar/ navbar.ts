0298faf235e1e401470efae75eab109090477188
davidcr08 <68881672+davidcr08@users.noreply.github.com>
2026-02-23 10:52:13 -0500

    add HUD log navbar

---
+++ src/app/core/navbar/ navbar.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.css'
+})
+export class Navbar {
+
+  constructor(private router: Router) {}
+
+  goToLogin() {
+    this.router.navigate(['/login']);
+  }
+
+  goToRegister() {
+    this.router.navigate(['/register']);
+  }
+}
